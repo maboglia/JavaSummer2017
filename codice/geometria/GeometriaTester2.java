76e2b51fbd3cb6b118d773cec89016702bbc9c6b
maboglia <mauro.bogliaccino@gmail.com>
2017-06-28 12:39:24 +0200

    geometria

---
+++ codice/geometria/GeometriaTester2.java
@@ -0,0 +1,40 @@
+package geometria;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GeometriaTester2 {
+
+	//costante che rappresenta 
+	final static int NUM_RIPETIZIONI = 500;
+	final static int NUM_PUNTI = 10;
+	static ArrayList<Punto> punti = new ArrayList<>();
+	static Random coordinata = new Random();
+	
+	public static void main(String[] args) {
+
+
+		Punto p1 = new Punto(2, 2);
+		Punto p2 = new Punto(5, 2);
+		Punto p3 = new Punto(5, 6);
+		
+		Segmento s = new Segmento(p1, p2);
+		
+		System.out.println( "Il segmento s è lungo "  +  s.lunghezza());
+		
+		Triangolo t = new Triangolo(p1, p2, p3);
+		
+		System.out.println(t.a.lunghezza());
+		System.out.println(t.b.lunghezza());
+		System.out.println(t.c.lunghezza());
+		
+		System.out.println("Il perimetro del triangolo t è: " + t.perimetro());
+		System.out.println("L'area del triangolo t è: " + t.area());
+		
+		
+		
+		
+		
+	}
+	
+}
